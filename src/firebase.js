ded1c6b8633e0e32085e17754eb6ba5ff08a2e45
Rokas <rookas.rudzenskas@gmail.com>
2021-06-14 12:39:43 +0300

    Rokas

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,16 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    // your Firebase credentials go here
-})
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        apiKey: "AIzaSyBs1v-oFpfKa68WfY9e_cDteQ0nZuwWs4k",
+        authDomain: "rookas-messenger-mern-app.firebaseapp.com",
+        projectId: "rookas-messenger-mern-app",
+        storageBucket: "rookas-messenger-mern-app.appspot.com",
+        messagingSenderId: "246603889387",
+        appId: "1:246603889387:web:5781733f562cf7486e5cae",
+        measurementId: "G-X6TWQQ4MVQ"
+});
 
-const db = firebaseApp.firestore()
+const db = firebaseApp.firestore();
 
-export default db
+export default db;
