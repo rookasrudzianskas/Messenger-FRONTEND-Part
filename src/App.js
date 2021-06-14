7602651229e0fbe222d4bfd230fac7adf2005084
Rokas <rookas.rudzenskas@gmail.com>
2021-06-14 15:05:19 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -2,35 +2,36 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core'
 import Message from './Message';
-import db from './firebase';
-import firebase from 'firebase'
 import FlipMove from 'react-flip-move'
 import SendIcon from '@material-ui/icons/Send'
 import IconButton from '@material-ui/core/IconButton'
+import axios from "./axios";
 
 function App() {
   const [input, setInput] = useState('')
   const [messages, setMessages] = useState([])
   const [username, setUsername] = useState('')
 
-  useEffect(() => {
-    db.collection('messages').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      setMessages(snapshot.docs.map(doc => ({ id: doc.id, message: doc.data() })))
-    })
-  }, [])
+
+    const sync = async () => {
+      await axios.get('/retrieve/conversation').then((res) => {
+          console.log(res.data);
+          setMessages(res.data);
+      })
+    }
+
+    useEffect(() => {
+        sync();
+    }, []);
 
   useEffect(() => {
-    setUsername(prompt('Please enter your name'))
+    setUsername(prompt('Please enter your name 🚀'))
   }, [])
 
   const sendMessage = (e) => {
     e.preventDefault()
 
-    db.collection('messages').add({
-      message: input,
-      username: username,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    })
+
 
     setInput('')
   }
@@ -51,8 +52,8 @@ function App() {
 
       <FlipMove>
         {
-          messages.map(({ id, message }) => (
-            <Message key={id} message={message} username={username} />
+          messages.map((message) => (
+            <Message key={message._id} message={message} username={username} />
           ))
         }
       </FlipMove>
